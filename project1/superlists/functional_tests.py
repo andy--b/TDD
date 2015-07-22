804f9747f82b37ad3d1aced08125f92745a6fc20
Andy <andrew.r.bowling@gmail.com>
2015-07-21 23:30:47 -0400

    Model for list Items and associated migration

--- project1/superlists/functional_tests.py
+++ project1/superlists/functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -33,20 +35,24 @@ class NewVisitorTest(unittest.TestCase):
 		
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Cook bacon' for row in rows)
-		)
+		self.assertIn('1: Cook bacon', [row.text for row in rows])
 
 		# There is still a text box inviting her to add another item. 
 		# He adds "Eat the bacon"
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Eat the bacon')	
+		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and shows him both items in the list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Cook bacon', [row.text for row in rows])
+		self.assertIn('2: Eat the bacon', [row.text for row in rows])
 
 		# Bob wonders whether the site will remember the list. Then he sees
 		# that the site has generated a unique URL for him -- there is some
 		# explanatory text to that effect
-
+		self.fail('Finish the test!')
 		# Bob visits that URL - his to-do list is still there
 
 		# Satisfied, Bob closes his browser, full of bacon
